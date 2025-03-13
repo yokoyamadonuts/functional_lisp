4a452d2824ba24d4574813199ffe3e05d97e1c93
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-03-13 22:06:57 +0900

    traverseの実装

--- traverse.lisp
+++ traverse.lisp
@@ -1,1 +1,6 @@
-traverse.lisp
+ (defun traverse (tree fun)
+    (if (null tree)
+        nil
+        (if (atom (car tree))
+            (cons (funcall fun (car tree)) (traverse (cdr tree) fun))
+            (cons (traverse (car tree) fun) (traverse (cdr tree) fun)) )))
