495f617d71c50d2a63dd0e85df3f6339560b11f4
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-03-13 22:13:53 +0900

    flat-traverseの実装

--- traverse.lisp
+++ traverse.lisp
@@ -4,3 +4,13 @@
         (if (atom (car tree))
             (cons (funcall fun (car tree)) (traverse (cdr tree) fun))
             (cons (traverse (car tree) fun) (traverse (cdr tree) fun)) )))
+
+(defun flat-traverse (tree fun)
+    (reverse (flat-traverse2 tree fun nil)))
+
+(defun flat-traverse2 (tree fun result)
+    (if (null tree)
+        result
+        (if (atom (car tree))
+            (flat-traverse2 (cdr tree) fun (cons (funcall fun (car tree)) result))
+            (flat-traverse2 (cdr tree) fun (flat-traverse2 (car tree) fun result)) ))
