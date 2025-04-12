aaf3eaf4ab4045a41373305898ee5df56938a73f
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-04-12 22:28:35 +0900

    infinite-n macroの実装

--- infinite_list.lisp
+++ infinite_list.lisp
@@ -11,5 +11,8 @@
 (defun inifinite-n (n)
     (delay-cons n (infinite-n (+ n 1))) )
 
+(defmacro infinite-n (n)
+    `(delay-cons ,n (infinite-n ,(+ n 1))) )
+
 ; (setf list0 (infinite-n 1))
 ; (1 .#<FUNCTION :LAMBDA NIL (INFINITE-N (+ N 1))>)
