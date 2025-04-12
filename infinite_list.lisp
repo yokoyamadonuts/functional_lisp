84fc5af294c8c4d92e48a47e40dfb36987e23bc0
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-04-12 22:23:21 +0900

    infinite_listの実装

---
+++ infinite_list.lisp
@@ -0,0 +1,15 @@
+(defmacro delay-cons (x y)
+    `(cons ,x (lambda () ,y)) )
+
+(defun delay-car (x)
+    (car x))
+
+(defun delay-cdr (x)
+    (let ((ret (funcall (cdr x))))
+        (setf (cdr x) ret) ))
+
+(defun inifinite-n (n)
+    (delay-cons n (infinite-n (+ n 1))) )
+
+; (setf list0 (infinite-n 1))
+; (1 .#<FUNCTION :LAMBDA NIL (INFINITE-N (+ N 1))>)
